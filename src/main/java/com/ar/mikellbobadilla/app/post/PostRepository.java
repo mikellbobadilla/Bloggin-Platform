4a41e8cc78af69eb989e6195699b93e5d8d33706
sting <bobadillasting@gmail.com>
2024-09-11 23:29:20 -0300

    Refactor PostRepository imports

--- src/main/java/com/ar/mikellbobadilla/app/post/PostRepository.java
+++ src/main/java/com/ar/mikellbobadilla/app/post/PostRepository.java
@@ -1,6 +1,5 @@
 package com.ar.mikellbobadilla.app.post;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
